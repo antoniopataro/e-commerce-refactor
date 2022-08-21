4e9c320670d03dd808a724b76b8b9204190941e1
antoniopataro <antonio.pataro12@gmail.com>
2022-08-21 13:58:46 -0300

    fix: sorted categories on sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,6 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import Checkbox from "../Checkbox";
-
-import Link from "next/link";
 
 import { useRouter } from "next/router";
 
@@ -21,11 +19,11 @@ interface Props {
 function Sidebar({ showSidebar, categories }: Props) {
   const router = useRouter();
 
+  const [tags, setTags] = useState<string[]>((router.query.tags as string[]) || []);
+
   const handleSlugBasedOnCategoryName = (categoryName: string) => {
     return categoryName.toLowerCase().replace(/ /g, "-");
   };
-
-  const [tags, setTags] = useState<string[]>((router.query.tags as string[]) || []);
 
   useEffect(() => {
     if (router.query.category) {
@@ -47,28 +45,39 @@ function Sidebar({ showSidebar, categories }: Props) {
     });
   }, [tags]);
 
+  const sortedCategories = categories.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  });
+
   return (
     <aside
       className={`${
         showSidebar ? "" : "hidden"
       } lg:flex flex-col w-full md:w-1/3 border-r-[1px] border-gray-300 overflow-hidden`}
     >
-      <nav className="flex flex-col gap-4 mx-16 py-16 border-b-[1px] border-gray-300 overflow-hidden">
+      <nav className="flex flex-col gap-4 mx-8 py-8 sm:mx-16 sm:py-16 border-b-[1px] border-gray-300 overflow-hidden">
         <span className="text-base font-semibold">Category</span>
-        {categories.map((category) => (
-          <Link href={`/${handleSlugBasedOnCategoryName(category.name)}`} key={category.name}>
-            <a className="w-fit pl-4 text-gray-300 transition-colors hover:text-violet-700">{category.name}</a>
-          </Link>
+        {sortedCategories.map((category) => (
+          <a
+            href={`/${handleSlugBasedOnCategoryName(category.name)}`}
+            key={category.name}
+            className="w-fit pl-4 text-gray-300 transition-colors hover:text-violet-700"
+          >
+            {category.name}
+          </a>
         ))}
       </nav>
-      <menu className="flex flex-col gap-4 mx-16 py-16 border-b-[1px] border-gray-300 overflow-hidden">
+      <menu className="flex flex-col gap-4 mx-8 py-8 sm:mx-12 sm:py-12 md:mx-16 md:py-16 border-b-[1px] border-gray-300 overflow-hidden">
         <span className="text-base font-semibold">Tags</span>
-        <Checkbox label="Men" tags={tags} setTags={setTags} />
-        <Checkbox label="Woman" tags={tags} setTags={setTags} />
         <Checkbox label="Blue" tags={tags} setTags={setTags} />
         <Checkbox label="Green" tags={tags} setTags={setTags} />
-        <Checkbox label="Red" tags={tags} setTags={setTags} />
+        <Checkbox label="Men" tags={tags} setTags={setTags} />
         <Checkbox label="Pink" tags={tags} setTags={setTags} />
+        <Checkbox label="Red" tags={tags} setTags={setTags} />
+        <Checkbox label="Woman" tags={tags} setTags={setTags} />
       </menu>
     </aside>
   );
